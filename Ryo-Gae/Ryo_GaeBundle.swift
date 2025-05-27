711520ccf62c9c03d3e2ae6d31971347cc8c5278
gui8787 <guilhermecolucci@gmail.com>
2025-05-28 00:30:36 +0900

    Extension

---
+++ Ryo-Gae/Ryo_GaeBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Ryo_GaeBundle.swift
+//  Ryo-Gae
+//
+//  Created by Guilherme Pereira on 5/28/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Ryo_GaeBundle: WidgetBundle {
+    var body: some Widget {
+        Ryo_Gae()
+    }
+}
