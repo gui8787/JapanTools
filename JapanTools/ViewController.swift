9f45ae9f470d7797b293ed58f9081b321cbeb1ef
gui8787 <guilhermecolucci@gmail.com>
2025-05-28 00:18:25 +0900

    Initial Commit

---
+++ JapanTools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JapanTools
+//
+//  Created by Guilherme Pereira on 5/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
